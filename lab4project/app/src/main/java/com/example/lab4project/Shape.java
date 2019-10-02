ac4c9ec504ed4294c282bd2a865a186aef22fe54
Abuni Amanda Gaiya <32043872+Abunie@users.noreply.github.com>
2019-10-01 19:21:24 -0600

    Update Shape.java

--- lab4project/app/src/main/java/com/example/lab4project/Shape.java
+++ lab4project/app/src/main/java/com/example/lab4project/Shape.java
@@ -3,8 +3,6 @@ package com.example.lab4project;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "yellow";
-
-
+    private String color = "red";
 }
 
