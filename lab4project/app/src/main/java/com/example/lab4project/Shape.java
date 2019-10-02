ca66b73bd273501bdb27e754bb271d0c8826f3e7
Abunie <amandaabuni_gaiya@yahoo.com>
2019-10-01 19:20:30 -0600

    Changing to blue

--- lab4project/app/src/main/java/com/example/lab4project/Shape.java
+++ lab4project/app/src/main/java/com/example/lab4project/Shape.java
@@ -3,7 +3,8 @@ package com.example.lab4project;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "yellow";
+    private String color = "blue";
+
 
 
 }
